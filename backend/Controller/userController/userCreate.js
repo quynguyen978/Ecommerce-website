65baa989793d938710e37d24b9fb67280ac65fd9
Quy Nguyen <louisnguyen110291@gmail.com>
2025-03-18 00:01:02 -0400

    update jwt

--- backend/Controller/userController/userCreate.js
+++ backend/Controller/userController/userCreate.js
@@ -1,6 +1,6 @@
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mailMain = require('../../services/mailer');
 const saltRounds = 10;
 
